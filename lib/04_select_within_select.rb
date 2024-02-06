47b709cb585f64270bb70f646c6fc41555f8dea2
Jerry Wang <ziyanwang1105@gmail.com>
2024-02-06 14:18:13 -0800

    finish 04 spec

--- lib/04_select_within_select.rb
+++ lib/04_select_within_select.rb
@@ -62,7 +62,7 @@ def richer_than_england
     countries
     WHERE
     name = 'United Kingdom'
-  );
+  ) and continent = 'Europe';
   SQL
 end
 
@@ -70,6 +70,17 @@ def neighbors_of_certain_b_countries
   # List the name and continent of countries in the continents containing
   # 'Belize', 'Belgium'.
   execute(<<-SQL)
+  SELECT
+  name, continent
+  FROM
+  countries
+  WHERE
+  continent in (
+    SELECT continent
+    FROM
+    countries
+    WHERE name in ('Belize', 'Belgium')
+  );
   SQL
 end
 
@@ -77,6 +88,19 @@ def population_constraint
   # Which country has a population that is more than Canada but less than
   # Poland? Show the name and the population.
   execute(<<-SQL)
+  SELECT
+  name, population
+  FROM
+  countries
+  WHERE
+  population > (
+    SELECT population
+    FROM countries
+    WHERE name ='Canada') AND population < (
+      SELECT population
+      FROM countries
+      WHERE name = 'Poland'
+    );
   SQL
 end
 
@@ -86,5 +110,15 @@ def sparse_continents
   # population.
   # Hint: Sometimes rewording the problem can help you see the solution.
   execute(<<-SQL)
+  SELECT
+  name, continent, population
+  FROM
+  countries
+  WHERE continent in (
+    SELECT continent
+    FROM countries
+    GROUP BY continent
+    HAVING MAX(population) < 25000000
+  );
   SQL
 end
